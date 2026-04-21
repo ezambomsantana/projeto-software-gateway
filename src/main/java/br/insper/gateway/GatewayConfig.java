fed06fdaaaacebbe72b270f0d7cb5bb0fdf2acb4
Eduardo Santana <ezambomsantana@gmail.com>
2026-04-21 17:22:31 -0300

    config urls

--- src/main/java/br/insper/gateway/GatewayConfig.java
+++ src/main/java/br/insper/gateway/GatewayConfig.java
@@ -1,5 +1,6 @@
 package br.insper.gateway;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -8,16 +9,23 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class GatewayConfig {
 
+
+    @Value("${services.user}")
+    private String userServiceUrl;
+
+    @Value("${services.connections}")
+    private String connectionsServiceUrl;
+
     @Bean
     public RouteLocator routes(RouteLocatorBuilder builder, TimingGatewayFilterFactory timing) {
         return builder.routes()
                 .route("user-route", r -> r.path("/user/**")
                         .filters(f -> f.stripPrefix(1)
                                 .filter(timing.apply(config(true, "user-route"))))
-                        .uri("http://localhost:5000"))
+                        .uri(userServiceUrl))
                 .route("connections-route", r -> r.path("/connections/**")
                         .filters(f -> f.stripPrefix(1))
-                        .uri("http://localhost:8080"))
+                        .uri(connectionsServiceUrl))
                 .build();
     }
 
